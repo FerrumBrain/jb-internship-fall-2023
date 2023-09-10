ed3f4ee395f98592e2887146b1c770e779992c41
FerrumBrain <lemur1416@gmail.com>
2023-09-10 12:59:12 +0200

    Solution

--- src/MSQueue.kt
+++ src/MSQueue.kt
@@ -1,15 +1,40 @@
 import java.util.concurrent.atomic.AtomicReference
 
 class MSQueue<E> : Queue<E> {
-    private val head: AtomicReference<Node<E>>
-    private val tail: AtomicReference<Node<E>>
+    private val node = Node<E>(null)
+    private val head: AtomicReference<Node<E>> = AtomicReference(node)
+    private val tail: AtomicReference<Node<E>> = AtomicReference(node)
 
     override fun enqueue(element: E) {
-        TODO("Implement me")
+        val node = Node(element)
+        while (true) {
+            val t = tail.get()
+            if (t.next.compareAndSet(null, node)) {
+                tail.compareAndSet(t, node)
+                break
+            }
+            tail.compareAndSet(t, t.next.get())
+        }
     }
 
     override fun dequeue(): E? {
-        TODO("implement me")
+        while (true) {
+            val h = head.get()
+            val t = tail.get()
+            val next = h.next.get()
+            if (h == t) {
+                if (next == null) {
+                    return null
+                }
+                tail.compareAndSet(t, next)
+            } else {
+                if (head.compareAndSet(h, next)) {
+                    val snapshot = next!!.element
+                    next.element = null
+                    return snapshot
+                }
+            }
+        }
     }
 
     // FOR TEST PURPOSE, DO NOT CHANGE IT.
